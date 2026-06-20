d012457fafaa1a2295dd3d698805c256b9e38213
rahuldakua01 <rahulkumardakua01@gmail.com>
2026-06-21 00:17:50 +0530

    Update import path for Base in seller.py

--- backend/models/seller.py
+++ backend/models/seller.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from db.base import Base
+from backend.db.base import Base
 
 
 class Seller(Base):
